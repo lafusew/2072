68dd4c943c2892dbec6be208b40c157357550d11
LaFusew <antoine.oddoz@gmail.com>
2022-03-26 11:12:43 +0100

    renderer setup

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,3 +1,29 @@
 export class Renderer {
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+  canvasWidth: number;
+  canvasHeight: number;
 
+  constructor(
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D
+  ) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.canvasWidth = canvas.width;
+    this.canvasHeight = canvas.height;
+  }
+
+  renderBackground(canvasImgSrc: CanvasImageSource): void {
+    this.ctx.moveTo(0, 0);
+    this.ctx.drawImage(canvasImgSrc, this.canvasWidth, this.canvasHeight)
+  }
+
+  renderBtc() {
+
+  }
+
+  renderNfts() {
+
+  }
 }
