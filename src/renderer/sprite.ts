47ee840869a2e1ff1aaee5ce0cd79618d2f20fa9
LaFusew <antoine.oddoz@gmail.com>
2022-03-27 10:22:24 +0200

    [+] Attack animation

--- src/renderer/sprite.ts
+++ src/renderer/sprite.ts
@@ -1,4 +1,4 @@
-import { allType, IEntity } from "../core/entities/entity";
+import { allState, allType, IEntity } from "../core/entities/entity";
 
 export class SpriteRenderer {
   ctx: CanvasRenderingContext2D;
@@ -26,6 +26,9 @@ export class SpriteRenderer {
   renderEntity(entity: IEntity) {
     switch (entity.type) {
       case allType.BTC:
+        if (entity.state === allState.ATTACK) {
+          this.ctx.rotate(10 * Math.PI / 18);
+        }
         this.ctx.drawImage(this.btc, entity.x - (entity.size / 2), entity.y - (entity.size / 2), entity.size, entity.size);
         break;
 
